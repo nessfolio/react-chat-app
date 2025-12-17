4d48fc4cf0409430d1023a9d75ae946231a2fe4e
ness <nessfolio@gmail.com>
2025-12-17 23:36:57 +0600

    Add App layout, background, and List/Chat/Detail components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import './App.scss'
+import './App.css'
+import List from './components/list/List.jsx'
+import Chat from './components/chat/Chat.jsx'
+import Detail from './components/detail/Detail.jsx'
 
 function App() {
 
   return (
-    <>
-      App.jsx
-    </>
+    <div className='container'>
+      <List />
+      <Chat />
+      <Detail />
+    </div>
   )
 }
 
