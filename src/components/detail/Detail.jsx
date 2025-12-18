9e6ccc4f70cd3573da430ebf3f5de7384bdac4b0
ness <nessfolio@gmail.com>
2025-12-19 02:48:26 +0600

    Stylyze and implement: Detail block

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -2,7 +2,78 @@ import './Detail.css'
 
 const Detail = () => {
   return (
-    <div className='detail'>Detail</div>
+    <div className='detail'>
+
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Jane Doe</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & Help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+
+        <div className="option">
+          <div className="title">
+            <span>Shared photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="./bg.jpg" alt="" />
+                <span>photo_2025.png</span>
+              </div>
+              <img src="./download.png" alt="" className='icon'/>
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="./bg.jpg" alt="" />
+                <span>photo_2025.png</span>
+              </div>
+              <img src="./download.png" alt="" className='icon'/>
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="./bg.jpg" alt="" />
+                <span>photo_2025.png</span>
+              </div>
+              <img src="./download.png" alt="" className='icon'/>
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="./bg.jpg" alt="" />
+                <span>photo_2025.png</span>
+              </div>
+              <img src="./download.png" alt="" className='icon'/>
+            </div>
+          </div>
+        </div>
+
+        <div className="option">
+          <div className="title">
+            <span>Shared files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+
+        <button>Block User</button>
+        <button className="logout">
+          Logout
+        </button>
+      </div>
+    </div>
   )
 }
 
